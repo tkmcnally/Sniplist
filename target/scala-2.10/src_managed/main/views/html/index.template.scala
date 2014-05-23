18b0befc2ce48af78af1c75ce3838a39ec99ca2b
Thomas McNally <tk.mcnally@gmail.com>
2014-05-23 18:22:10 -0400

    favicon update

--- target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -25,15 +25,16 @@ Seq[Any](format.raw/*1.19*/("""
 
 <head>
     <link href=""""),_display_(Seq[Any](/*6.18*/routes/*6.24*/.Assets.at("stylesheets/main.css"))),format.raw/*6.58*/("""" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href=""""),_display_(Seq[Any](/*7.38*/routes/*7.44*/.Assets.at("images/favicon.ico"))),format.raw/*7.76*/(""""/>
     <title>Sniplist</title>
 </head>
 
 <html>
     <body>
         <div class="middle-wrapper">
-           <h1>Sniplist</h1>
-           <h2>under construction.</h2><br>
-            <img style="padding-right: 3%" src=""""),_display_(Seq[Any](/*15.50*/routes/*15.56*/.Assets.at("images/music.png"))),format.raw/*15.86*/("""">
+            <h1>Sniplist</h1>
+            <h2>under construction.</h2>
+            <img style="padding-right: 3%" src=""""),_display_(Seq[Any](/*16.50*/routes/*16.56*/.Assets.at("images/music.png"))),format.raw/*16.86*/("""">
         </div>
     </body>
 </html>
@@ -51,11 +52,11 @@ Seq[Any](format.raw/*1.19*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Fri May 23 18:06:24 EDT 2014
+                    DATE: Fri May 23 18:19:58 EDT 2014
                     SOURCE: C:/Projects/Sniplist/app/views/index.scala.html
-                    HASH: bc42e18d7b4d6baf2b797ad101599f5c0dda7fca
-                    MATRIX: 556->1|667->18|745->61|759->67|814->101|1102->353|1117->359|1169->389
-                    LINES: 19->1|22->1|27->6|27->6|27->6|36->15|36->15|36->15
+                    HASH: 88d9dac482fa3a7fe4b28d01733a73e8d00f3710
+                    MATRIX: 556->1|667->18|745->61|759->67|814->101|924->176|938->182|991->214|1243->430|1258->436|1310->466
+                    LINES: 19->1|22->1|27->6|27->6|27->6|28->7|28->7|28->7|37->16|37->16|37->16
                     -- GENERATED --
                 */
             
--- target/scala-2.10/src_managed/main/views/html/main.template.scala
+++ target/scala-2.10/src_managed/main/views/html/main.template.scala
@@ -27,7 +27,7 @@ Seq[Any](format.raw/*1.32*/("""
     <head>
         <title>"""),_display_(Seq[Any](/*7.17*/title)),format.raw/*7.22*/("""</title>
         <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*8.54*/routes/*8.60*/.Assets.at("stylesheets/main.css"))),format.raw/*8.94*/("""">
-        <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*9.59*/routes/*9.65*/.Assets.at("images/favicon.png"))),format.raw/*9.97*/("""">
+        <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*9.59*/routes/*9.65*/.Assets.at("../../public/images/favicon.ico"))),format.raw/*9.110*/("""">
         <script src=""""),_display_(Seq[Any](/*10.23*/routes/*10.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*10.74*/("""" type="text/javascript"></script>
     </head>
     <body>
@@ -46,10 +46,10 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Fri May 23 16:32:59 EDT 2014
+                    DATE: Fri May 23 18:16:13 EDT 2014
                     SOURCE: C:/Projects/Sniplist/app/views/main.scala.html
-                    HASH: c98f5ae2f9ce07d5fd682f94f4cdd90b31fc141e
-                    MATRIX: 560->1|684->31|772->84|798->89|895->151|909->157|964->191|1060->252|1074->258|1127->290|1188->315|1203->321|1270->366|1373->433|1402->440
+                    HASH: 5a9ae248ec59a15053350b498626ba6282a96103
+                    MATRIX: 560->1|684->31|772->84|798->89|895->151|909->157|964->191|1060->252|1074->258|1141->303|1202->328|1217->334|1284->379|1387->446|1416->453
                     LINES: 19->1|22->1|28->7|28->7|29->8|29->8|29->8|30->9|30->9|30->9|31->10|31->10|31->10|34->13|34->13
                     -- GENERATED --
                 */
